821775f01441d5eb85324d11731fd2282e43db6b
José Peraza <joseperazarivero@gmail.com>
2021-04-25 22:48:21 -0500

    finish desing

---
+++ src/components/FormComponentUser.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Title from './Title';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
+
+export default function FormComponent() {
+  return (
+    <React.Fragment>
+    <Title>Actualizar Usuario</Title>
+        <FormControl>
+            <TextField
+            id="first-name"
+            label="Primer nombre"
+            style={{ margin: 8 }}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+                shrink: true,
+            }}
+            />
+
+            <TextField
+            id="last-name"
+            label="Segundo nombre"
+            style={{ margin: 8 }}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+                shrink: true,
+            }}
+            />   
+
+            <TextField
+            id="email"
+            label="Email"
+            style={{ margin: 8 }}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+                shrink: true,
+            }}
+            />      
+
+            <TextField
+            id="telephone"
+            label="Telefono"
+            style={{ margin: 8 }}
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+                shrink: true,
+            }}
+            />  
+            <Button variant="contained" color="primary">
+                Actualizar
+            </Button> 
+        </FormControl>
+    </React.Fragment>
+  );
+}
---
+++ src/components/Users.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import Link from '@material-ui/core/Link';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Title from './Title';
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+// Generate Order Data
+function createData(id, date, name, shipTo, paymentMethod, amount) {
+  return { id, date, name, shipTo, paymentMethod, amount };
+}
+
+const rows = [
+  createData(0, '16 Mar, 2019', 'Elvis Presley', 'Tupelo, MS', 'VISA ⠀•••• 3719', 312.44),
+  createData(1, '16 Mar, 2019', 'Paul McCartney', 'London, UK', 'VISA ⠀•••• 2574', 866.99),
+  createData(2, '16 Mar, 2019', 'Tom Scholz', 'Boston, MA', 'MC ⠀•••• 1253', 100.81),
+  createData(3, '16 Mar, 2019', 'Michael Jackson', 'Gary, IN', 'AMEX ⠀•••• 2000', 654.39),
+  createData(4, '15 Mar, 2019', 'Bruce Springsteen', 'Long Branch, NJ', 'VISA ⠀•••• 5919', 212.79),
+];
+
+function preventDefault(event) {
+  event.preventDefault();
+}
+
+const useStyles = makeStyles((theme) => ({
+  seeMore: {
+    marginTop: theme.spacing(3),
+  },
+}));
+
+export default function Orders() {
+
+    const [clients, setClients] = useState([]);
+
+
+
+    // Llamar Api
+    useEffect(() => {
+        const promise = axios(`https://cors-anywhere.herokuapp.com/https://mighty-mesa-47222.herokuapp.com/clientes/get/`);
+
+        promise.then((res) => {
+            setClients(res.data);
+        });
+    },[]);
+
+    useEffect(() => {
+        console.log(clients);
+    },[clients]);
+
+
+  const classes = useStyles();
+  return (
+    <React.Fragment>
+      <Title>Usuarios activos</Title>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <TableCell>Nombre</TableCell>
+            <TableCell>Apellido</TableCell>
+            <TableCell>Email</TableCell>
+            <TableCell>Telefono</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {clients.map((value) => (
+            <TableRow key={value.id}>
+              <TableCell>{value.first_name}</TableCell>
+              <TableCell>{value.last_name}</TableCell>
+              <TableCell>{value.email}</TableCell>
+              <TableCell>{value.telephone}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      <div className={classes.seeMore}>
+        <Link color="primary" href="#" onClick={preventDefault}>
+          See more orders
+        </Link>
+      </div>
+    </React.Fragment>
+  );
+}
